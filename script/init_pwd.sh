fdf90b7d194f5ecdbb88d309ce99cfbcb956d170
root <root@ip-172-31-36-123.ap-southeast-1.compute.internal>
2023-02-13 14:48:25 +0000

    Changed Configuration

--- script/init_pwd.sh
+++ script/init_pwd.sh
@@ -4,8 +4,8 @@ echo "your password is:$PASSWORD"
 echo "your minio endPoint is:$MINIO_ENDPOINT"
 
 sed -i "/^\([[:space:]]*dbMysqlUserName: *\).*/s//\1$USER/;0,/\([[:space:]]*dbUserName: *\).*/s//\1 $USER/;/\([[:space:]]*accessKeyID: *\).*/s//\1$USER/;/\([[:space:]]*endpoint: *\).*/s//\1\"abc\"/;" ../config/usualConfig.yaml
-sed -i "/^\([[:space:]]*dbMysqlPassword: *\).*/s//\1$PASSWORD/;/\([[:space:]]*dbPassword: *\).*/s//\1$PASSWORD/;/\([[:space:]]*secret: *\).*/s//\1$PASSWORD/;/\([[:space:]]*secretAccessKey: *\).*/s//\1$PASSWORD/;" ../config/usualConfig.yaml
+sed -i "/^\([[:space:]]*dbMysqlPassword: *\)\".*/s//\1$MYSQL_PASSWORD/;\"/\([[:space:]]*dbPassword: *\)\".*/s//\1$MONGO_PASSWORD/;\"/\([[:space:]]*secret: *\)\".*/s//\1$PASSWORD/;\"/\([[:space:]]*secretAccessKey: *\)\".*/s//\1$MINIO_SECRET/;\"" ../config/usualConfig.yaml
 
 sed -i "/\([[:space:]]*endpoint: *\).*/s##\1$MINIO_ENDPOINT#;" ../config/usualConfig.yaml
-sed -i "/\([[:space:]]*dbPassWord: *\).*/s//\1$PASSWORD/;" ../config/usualConfig.yaml
-sed -i "/\([[:space:]]*secret: *\).*/s//\1$PASSWORD/;" ../docker-compose_cfg/config.yaml
+sed -i "/\([[:space:]]*dbPassWord: *\)\".*/s//\1$REDIS_PASSWORD/;\"" ../config/usualConfig.yaml
+sed -i "/\([[:space:]]*secret: *\)\".*/s//\1$PASSWORD/;\"" ../docker-compose_cfg/config.yaml
